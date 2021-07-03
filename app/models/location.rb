307c782916bca293eb9ee614ea120873e84017e2
dsusviela <d.susviela@gmail.com>
2021-07-03 18:51:46 -0300

    Add ordered_by_name method and spec to Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
   belongs_to :region
   has_many :people
+
+  def self.ordered_by_name
+    Location.all.order(:name)
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,4 +1,21 @@
 require "spec_helper"
 
 describe Location do
+  describe ".ordered_by_name" do
+    it "orders the locations based on alpahbetical order" do
+      locations = [
+        create(:location, name: "location1"),
+        create(:location, name: "location3"),
+        create(:location, name: "location2")
+      ]
+
+      result = Location.ordered_by_name
+
+      expect(result.pluck(:name)).to eq(%w(
+        location1
+        location2
+        location3
+      ))
+    end
+  end
 end
